cc01fdabf5291f35a4ec64d9a876556462e2f051
Kindstep <kindstep@arcada.fi>
2022-09-26 21:34:19 +0300

    Ett nytt försök av att göra en login komponent

---
+++ login/src/views/profile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="profile">
+        <h1>Profile Page</h1>
+        <p>
+            This is your profile page!
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'profile',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+    #secure {
+        background-color: #af1896;
+        border: 1px solid #020000;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
