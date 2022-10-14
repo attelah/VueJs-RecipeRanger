c7e97a2666cbc98f3092a9b2bff5a3d35a59a6dc
haanpaas <haanpaas@arcada.fi>
2022-10-14 10:39:03 +0300

    Merge branch 'main' of https://github.com/streamerteach/projekt2-foodwarriorz

---
+++ login/src/components/profile.vue
@@ -0,0 +1,21 @@
+<template>
+
+        <h1>{{title}}</h1>
+            
+</template>
+
+<script>
+    export default {
+        name: 'profile-vue',     
+        props:['title', 'content']
+            };     
+</script>
+
+<style scoped>
+    #secure {
+        background-color: #af1896;
+        border: 1px solid #020000;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
